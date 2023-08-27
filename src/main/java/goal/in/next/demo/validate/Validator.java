cdea5b4860201cfdf29d642334f543468dcff97c
nyoongoon <nyoongoon@naver.com>
2023-08-27 19:32:04 +0900

    초기 코드값 & 검증단 수정

--- src/main/java/goal/in/next/demo/validate/Validator.java
+++ src/main/java/goal/in/next/demo/validate/Validator.java
@@ -9,12 +9,15 @@ public class Validator {
     public void validateCategory(PostForm postForm) {
         String categoryCode = postForm.categoryCode();
 
-        if (!categoryCode.equals("CT000001") && !categoryCode.equals("CT000002")) {
+        if (categoryCode.equals("CT000001") ||
+                categoryCode.equals("CT000002") ||
+                categoryCode.equals("CT000003") ||
+                categoryCode.equals("CT000004")) {
             throw new IllegalArgumentException("잘못된 카테고리입니다");
         }
     }
 
-    public void validateExpenditureCode(PostForm postForm){
+    public void validateExpenditureCode(PostForm postForm) {
 
     }
 }
