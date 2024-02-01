95e59f2f1d87a9fc5a7b106b772df426a445b70f
nyoongoon <nyoongoon@naver.com>
2024-02-02 07:51:37 +0900

    예시추가

--- src/main/java/goal/in/next/demo/oop_example/interfaces/BreathingUnderwater.java
+++ src/main/java/goal/in/next/demo/oop_example/interfaces/BreathingUnderwater.java
@@ -1,5 +1,5 @@
 package goal.in.next.demo.oop_example.interfaces;
 
-public interface BreathingUnderwater extends Breathing {
+public interface BreathingUnderwater extends Breathing, Diving {
     void openGills(); // 아가미를 벌린다..
 }
---
+++ src/main/java/goal/in/next/demo/oop_example/interfaces/Diving.java
@@ -0,0 +1,5 @@
+package goal.in.next.demo.oop_example.interfaces;
+
+public interface Diving {
+    void dive(); // 잠수하다
+}
