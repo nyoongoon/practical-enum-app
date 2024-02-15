d98805bc7e0d4fe7bce90b55ca761e86e7f4a56e
nyoongoon <nyoongoon@naver.com>
2024-02-15 22:30:40 +0900

    중력과 저울 비교

--- src/main/java/goal/in/next/demo/oop_example/_3_clazz/Gravity.java
+++ src/main/java/goal/in/next/demo/oop_example/_3_clazz/Gravity.java
@@ -1,8 +1,8 @@
 package goal.in.next.demo.oop_example._3_clazz;
 
-public final class Gravity {
-    // 중력 상수
-    static final double GRAVITY_CONSTANT = 9.8;
+public final class Gravity { //중력을 무엇으로 상속할까... 추상화, 상속, 다형성 불가.. !
+    // 중력 상수 - 중력의 성질을 나타내지만, 어떤 대상의 순간적인 상태 나타내는 것이 아님!
+    static final double GRAVITY_CONSTANT = 9.8; // 상수만 있을 뿐, 상태 없음!
     // 대상의 무게 구하기
     static double getWeight(Object object){
         return object.mess * GRAVITY_CONSTANT;
@@ -14,15 +14,20 @@ public final class Gravity {
     class Banana {
         double mess = 0.3;
     }
-    class BalanceScale{
-        Object left;
-        Object right;
+    class BalanceScale implements Scale{ //추상화, 상속, 다형성 있음.
+        Object left; // 캡슐화 -> 상태 있음
+        Object right; // 특정 대상의 가변적인 순간적인 상태를 갖고 있을 수 있다..
 
-        Object putObjectsOnScale(Object left, Object right){
-            if(left.mess > right.mess){
-                return left;
+        void putObjectsOnScale(Object left, Object right){
+            this.left = left;
+            this.right = right;
+        }
+
+        Object getDownObject(){ // 명칭 다시
+            if(this.left.mess > this.right.mess){
+                return this.left;
             }else{
-                return right;
+                return this.right;
             }
         }
     }
@@ -55,7 +60,7 @@ public final class Gravity {
 
         // 객체지향의 세계
         BalanceScale balanceScale = new BalanceScale();
-        Object heavierObject = balanceScale.putObjectsOnScale(apple, banana);
+        heavierObject = balanceScale.putObjectsOnScale(apple, banana);
         System.out.println(heavierObject + "가 더 무거움");
     }
 }
